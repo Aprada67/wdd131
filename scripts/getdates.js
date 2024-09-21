0ab3ad1fac27441f2c8f0fdabe292e8614cd2e8c
AlbertoPrada66 <pra24007@byui.edu>
2024-09-21 15:19:36 +0100

    Home page finished

---
+++ scripts/getdates.js
@@ -0,0 +1,10 @@
+// Get Actual Year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = `© ${currentYear}`;
+
+// Get Last modified
+const lastModifiedDate = new Date(document.lastModified);
+const options = {year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: false};
+const formattedDate = lastModifiedDate.toLocaleString("en-US", options);
+
+document.getElementById("lastmodified").textContent = `Last modified: ${formattedDate}`;
